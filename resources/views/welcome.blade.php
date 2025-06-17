481719d5e058c1f51f9a40982a08033eeb47eecd
Gian2310 <carrenogarciagianpierre@gmail.com>
2025-06-17 14:10:20 +0200

    commit 14:09 17/06/25

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,16 @@
- <x-main>       
+ <x-main>  
+    <x-slot name="css">
+    <style>
+      .card-img-top{
+        height: 200px !important;
+        object-fit cover;
+      }
+    </style>
+    </x-slot>     
     <h1 class="mb-4 text-center">I voli disponibili</h1>
     <div class="row equal-height-cards">
-    @foreach ($flights as $flight)
-        <div class="col-3 mb-4">
-            <div class="card h-100" >
-                <img src="{{ $flight['cover'] }}" class="card-img-top" style="height: 200px !important;object-fit: cover;" alt="Foto di {{ $flight['destination'] }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $flight['destination'] }}</h5>
-                        <p class="card-text">
-                            Partenza: {{ $flight['time']['departure'] }}<br>
-                            Arrivo: {{ $flight['time']['arrival'] }}
-                        </p>
-                    <a href="/Dettagli/{{ $flight['id'] }}" class="btn btn-primary">Dettagli</a>
-                    </div>
-            </div>
-        </div>
-    @endforeach
+        @foreach ($flights as $flight)
+            <x-cardHomepage :flight="$flight"></x-cardHomepage>
+        @endforeach
     </div> 
 </x-main>
